1ec580253daed29294a67375f658ac10eabb13e8
Marijus <electromenn@gmail.com>
2023-07-11 16:43:41 +0300

    add hardcoded api url

--- src/api/service.js
+++ src/api/service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT;
+const API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT || 'http://ec2-13-48-133-198.eu-north-1.compute.amazonaws.com:3001/';
 
 const api = axios.create({
   baseURL: API_ENDPOINT,
